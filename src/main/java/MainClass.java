3459bb88d11269b17c106d9b4f34297b1351fa92
omer412 hemo <omer412 hemo>
2017-03-16 20:00:33 +0200

    first commit to GitHub

---
+++ src/main/java/MainClass.java
@@ -0,0 +1,16 @@
+/**
+ * Created by hemoomer on 16/03/2017.
+ */
+import java.util.Scanner;
+
+public class MainClass {
+    public static void main(String[] args) {
+        System.out.println("hellow world, how are you today?");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        System.out.println("the next number is:");
+        System.out.println(num+1);
+
+    }
+
+}
